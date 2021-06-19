01043c6b25509d6e1b45b60391d681cbf03b869a
Michael Kagnew <mike.kagnew@gmail.com>
2021-06-19 19:10:07 -0400

    adding more form functionality

--- SEG3125-lab05/scripts/experts.js
+++ SEG3125-lab05/scripts/experts.js
@@ -1,3 +1,24 @@
+services = {
+    Movement: "MovementR",
+    Aqua: "AquaT",
+    Chiro: "Chiro",
+    Acup: "Acpuncture",
+    Massage: "Massage"
+}
+
+
 expertInfo = {
-    
+    Michael: {
+        days: [1, 2,3],
+        services: [services["Aqua"], services["Acup"], services["Movement"]]
+    }, 
+    Bianca: {
+        days: [4, 5],
+        services: [services["Massage"], services["Chiro"], services["Movement"]]
+    },
+
+    Nikola: {
+        days: [1, 3, 5],
+        services: [services["Acup"], services["Massage"], services["Chiro"]]
+    },
 }
--- SEG3125-lab05/scripts/main.js
+++ SEG3125-lab05/scripts/main.js
@@ -1,7 +1,92 @@
 
 
 $(document).ready(function(){
-    $( "#datepicker" ).datepicker();
+    
+     if($("input[type='radio'][name='specialist']").is(":checked")){
+
+
+        $('input.timepicker').timepicker({}); //CHANGE THIS
+
+     } else {
+        $( "#datepicker" ).datepicker({
+            minDate: 0
+        });
+
+        $('input.timepicker').timepicker({}); 
+
+     }
+
+    $("input[type='radio']").checkboxradio();
+    $("input[type='checkbox']").checkboxradio();
+    //Setting the values for each service input dynamically here
+    $("#MoveR").val(services["Movement"]);
+    $("#Massage").val(services["Massage"]);
+    $("#AquaTherapy").val(services["Aqua"]);
+    $("#Chiro").val(services["Chiro"]);
+    $("#Acupuncture").val(services["Acup"]);
+
+    //Listen to service click, manipulate specialist list
+    $("#service_form input").click(function(event){
+        let services_selected = [];
+        $('#service_form input:checked').each(function() {
+            services_selected.push($(this).val());
+        });
+        showSpecialist(services_selected);        
+    });
+
+
+
 
 
 });
+
+function showSpecialist(services_selected){
+    for(let expert in expertInfo){
+        //console.log(expert);
+        let flag = true;
+        services_for_expert = expertInfo[expert]["services"];
+        console.log(services_for_expert);
+        console.log(services_selected);
+
+        for (let i =0; i < services_selected.length; i++){
+            temp_flag = services_for_expert.includes(services_selected[i]);
+            // for(let x =0; x < services_for_expert.length; x++)
+            if (temp_flag == false){
+                flag = false;
+                $("#"+expert).parent().hide();
+            } 
+        }
+        if (flag == true){
+            $("#"+expert).parent().show();
+
+        }
+
+    }
+
+}
+
+function SpecialistSelection(){
+    
+    $( "#datepicker" ).datepicker("option", "disabledDates", );
+
+
+}
+
+
+function disabledDates(day){
+
+    let specialist = $("input[type='radio'][name='specialist']:checked").val();
+    // let specialist = $("#specialists input:checked").val();
+    // let specialist = $("#specialists input:checked").attr("value");
+    console.log(specialist)
+    specialist_days = expertInfo[specialist]["days"];
+    
+    let flag = false;
+    for (let i = 0; i < specialist_days.length; i++){
+        if (specialist_days[i] == day.getDay()){
+            flag = true;
+        }
+    }
+    return [flag];
+}
+  
